a954a35e8f98312d36b6c6fffa651fece5e25b7d
saber <654360340@qq.com>
2018-04-23 13:17:15 +0800

    使用swipeRefresh

--- app/src/main/java/com/huburt/app/huburt/business/mzitu/list/MzituFragment.kt
+++ app/src/main/java/com/huburt/app/huburt/business/mzitu/list/MzituFragment.kt
@@ -5,7 +5,8 @@ import android.arch.lifecycle.ViewModelProviders
 import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.TabLayout
-import android.support.v7.widget.StaggeredGridLayoutManager
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.GridLayoutManager
 import android.view.View
 import android.widget.ImageView
 import com.blankj.utilcode.util.ConvertUtils
@@ -22,7 +23,6 @@ import com.huburt.app.huburt.bean.MeiZiTu
 import com.huburt.app.huburt.business.mzitu.detail.MzituDetailActivity
 import com.huburt.app.huburt.util.DividerGridItemDecoration
 import com.huburt.app.huburt.util.buildMzituUrl
-import com.scwang.smartrefresh.layout.header.ClassicsHeader
 import kotlinx.android.synthetic.main.fragment_mzitu.*
 
 /**
@@ -41,6 +41,10 @@ class MzituFragment : BaseFragment() {
     private lateinit var mzituViewModel: MzituViewModel
     private var page: Int = 0
     private var currentType: Int = 0
+    private val onRefreshListener = SwipeRefreshLayout.OnRefreshListener {
+        page = 0
+        mzituViewModel.getData(currentType, page)
+    }
 
     private val adapter = object : BaseQuickAdapter<MeiZiTu, BaseViewHolder>(R.layout.item_mzitu) {
         override fun convert(helper: BaseViewHolder, item: MeiZiTu) {
@@ -48,7 +52,7 @@ class MzituFragment : BaseFragment() {
             ImageLoaderManager.getInstance().load(imageView, buildMzituUrl(item.thumbUrl),
                     ImageOptions.newInstance().placeHolder(R.drawable.ic_insert_photo_black_24dp))
 
-            val height: Int = item.height * (ScreenUtils.getScreenWidth() / 2) / item.width
+            val height: Int = item.height * (ScreenUtils.getScreenWidth() / 3) / item.width
             val layoutParams = helper.itemView.layoutParams
             layoutParams.height = height
             helper.itemView.layoutParams = layoutParams
@@ -63,7 +67,7 @@ class MzituFragment : BaseFragment() {
         super.onCreate(savedInstanceState)
         mzituViewModel = ViewModelProviders.of(this)[MzituViewModel::class.java]
         mzituViewModel.result.observe(this, Observer<BaseResult<List<MeiZiTu>>> {
-            refresh_layout.finishRefresh()
+            swipe_refresh.isRefreshing = false
             adapter.loadMoreComplete()
             val list = it?.data
             if (page == 0) {
@@ -80,18 +84,18 @@ class MzituFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        refresh_layout.refreshHeader = ClassicsHeader(context);
-        refresh_layout.isEnableLoadmore = false
-        refresh_layout.setOnRefreshListener {
-            page = 0
-            mzituViewModel.getData(currentType, page)
+        swipe_refresh.setColorSchemeResources(R.color.colorAccent, R.color.colorPrimary)
+        swipe_refresh.setOnRefreshListener(onRefreshListener)
+        swipe_refresh.post {
+            swipe_refresh.isRefreshing = true
+            onRefreshListener.onRefresh()
         }
 
         adapter.setOnLoadMoreListener(BaseQuickAdapter.RequestLoadMoreListener {
             mzituViewModel.getData(currentType, ++page)
         }, recycler_view)
         recycler_view.adapter = adapter
-        recycler_view.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recycler_view.layoutManager = GridLayoutManager(context, 3)
         recycler_view.addOnItemTouchListener(object : OnItemClickListener() {
             override fun onSimpleItemClick(adapter: BaseQuickAdapter<*, *>, view: View, position: Int) {
                 val meiZiTu = this@MzituFragment.adapter.data[position]
@@ -111,13 +115,9 @@ class MzituFragment : BaseFragment() {
 
             override fun onTabSelected(tab: TabLayout.Tab) {
                 currentType = titles.indexOf(tab.text)
-                refresh_layout.autoRefresh()
+                swipe_refresh.isRefreshing = true
+                onRefreshListener.onRefresh()
             }
         })
-    }
-
-    override fun onResume() {
-        super.onResume()
-        refresh_layout.autoRefresh()
     }
 }
