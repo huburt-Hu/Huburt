801e93c54d5dd11230ff355ec8a0c5fc8fb29a16
saber <654360340@qq.com>
2018-06-05 10:02:52 +0800

    使用buildSrc使依赖可提示

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,13 @@
+object Versions {
+    val support_lib = "26.1.0"
+    val retrofit = "2.3.0"
+    val rxjava = "2.1.9"
+}
+
+object Libs {
+    val support_annotations = "com.android.support:support-annotations:${Versions.support_lib}"
+    val support_appcompat_v7 = "com.android.support:appcompat-v7:${Versions.support_lib}"
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val retrofit_rxjava_adapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+}
