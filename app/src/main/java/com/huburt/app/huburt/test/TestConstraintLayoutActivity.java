a71a2989df47dd656e68fa59b13c4b8ba1d1ae91
saber <654360340@qq.com>
2018-07-05 10:12:32 +0800

    test constraint layout

---
+++ app/src/main/java/com/huburt/app/huburt/test/TestConstraintLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.huburt.app.huburt.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.huburt.app.huburt.R;
+
+public class TestConstraintLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_constraint_layout);
+    }
+}
